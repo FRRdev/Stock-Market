589c88036f32e03aff68d0541d35c48140d4ff4b
Mikhail <mixail.critsyn@mail.ru>
2022-01-31 16:22:13 +0300

    put method for product

--- src/product/api.py
+++ src/product/api.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Form, UploadFile, File, Depends
+from fastapi import APIRouter, Form, UploadFile, File, Depends, Query
 import datetime
 from typing import List
 from fastapi_pagination import Page, paginate, Params
 
 from src.user.models import User
 from src.user.auth import current_active_user
-from .services import save_product
+from .services import save_product, update_product
 from .schemas import UploadProduct, ListProduct
 from .models import Product
 
@@ -20,12 +20,41 @@ async def create_product(
         deadline_time: datetime.datetime = Form(...),
         user: User = Depends(current_active_user)
 ):
+    """Create product router
+    """
     return await save_product(user, name, image, start_price, deadline_time)
+
+
+@product_router.put('/{product_pk}', status_code=201)
+async def update_product_by_pk(
+        product_pk: int = Query(...),
+        name: str = Form(...),
+        image: UploadFile = File(...),
+        start_price: float = Form(...),
+        deadline_time: datetime.datetime = Form(...),
+        user: User = Depends(current_active_user)
+):
+    """Update product router
+    """
+    return await update_product(product_pk, user, name, image, start_price, deadline_time)
+
+
+@product_router.get('/{user_pk}', response_model=Page[ListProduct])
+async def get_list_author_products(
+        user_pk: str,
+        user: User = Depends(current_active_user)
+):
+    """Get list products by author's id
+    """
+    queryset = await Product.objects.select_related("user").filter(user=user_pk).all()
+    return paginate(queryset)
 
 
 @product_router.get('', response_model=Page[ListProduct])
 async def get_list_all_videos(
         user: User = Depends(current_active_user)
 ):
+    """Get list of all products
+    """
     queryset = await Product.objects.select_related("user").all()
     return paginate(queryset)
--- src/product/services.py
+++ src/product/services.py
@@ -1,6 +1,9 @@
 from uuid import uuid4
+import ormar
+import os
 
 from fastapi import UploadFile, HTTPException
+from fastapi.responses import JSONResponse
 import datetime
 import aiofiles
 
@@ -9,12 +12,13 @@ from .models import Product
 from .schemas import UploadProduct
 
 
-async def save_product(user: User,
-                       name: str,
-                       img: UploadFile,
-                       start_price: float,
-                       deadline_time: datetime.datetime
-                       ):
+async def save_product(
+        user: User,
+        name: str,
+        img: UploadFile,
+        start_price: float,
+        deadline_time: datetime.datetime
+):
     file_name = f'media/img/{user.id}_{uuid4()}.jpg'
     if img.content_type == "image/jpeg":
         # background_tasks.add_task(write_video, file_name, file)
@@ -23,6 +27,31 @@ async def save_product(user: User,
         raise HTTPException(status_code=418, detail='It is not jpg')
     info = UploadProduct(name=name, start_price=start_price, deadline_time=deadline_time)
     return await Product.objects.create(image=file_name, user=user.dict(), **info.dict())
+
+
+async def update_product(
+        product_pk: int,
+        user: User,
+        name: str,
+        img: UploadFile,
+        start_price: float,
+        deadline_time: datetime.datetime
+):
+    try:
+        depricated_product = await Product.objects.get(pk=product_pk)
+    except ormar.exceptions.NoMatch:
+        return JSONResponse({"error": "This product with id doesn't exist'"})
+    image_to_delete = depricated_product.image
+    if os.path.exists(image_to_delete):
+        os.remove(image_to_delete)
+    file_name = f'media/img/{user.id}_{uuid4()}.jpg'
+    if img.content_type == "image/jpeg":
+        # background_tasks.add_task(write_video, file_name, file)
+        await write_img(file_name, img)
+    else:
+        raise HTTPException(status_code=418, detail='It is not jpg')
+    info = UploadProduct(name=name, start_price=start_price, deadline_time=deadline_time)
+    return await depricated_product.update(image=file_name, user=user.dict(), **info.dict())
 
 
 async def write_img(file_name: str, file: UploadFile):
